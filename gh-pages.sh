5013bb3f442584b9f504a159f8dd1cf405d5e3b8
Stefan Bühler <stbuehler@web.de>
2013-10-16 11:31:52 +0200

    add gh-pages.sh script to build gh-pages/ subdirectory

---
+++ gh-pages.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+self=$(readlink -f "$0")
+base=$(dirname "${self}")
+
+cd "${base}"
+
+rm -rf gh-pages/*
+
+cabal haddock --html-location='http://hackage.haskell.org/packages/archive/$pkg/latest/doc/html' --hyperlink-source --haddock-option="--built-in-themes"
+
+cp -ar dist/doc/html/nettle/* gh-pages/
